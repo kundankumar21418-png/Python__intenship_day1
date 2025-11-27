cecae35e72fb3fcea95878db78d7f10b74dcb331
kundankumar21418-png <kundankumar21418@gmail.com>
2025-11-27 18:04:34 +0530

    Add files via upload
    this is a basic calculater

---
+++ basic calculate.py
@@ -0,0 +1,8 @@
+# Basic Calculator
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
